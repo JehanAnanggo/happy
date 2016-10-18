b6b13f387ae4d97501a405a7a700640cbf1ed211
JehanAnanggo <jehanbernalananggo@gmail.com>
2016-10-19 03:07:41 +0800

    My Flask say Hello to You.

--- flaskapp.py
+++ flaskapp.py
@@ -1,7 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
 @app.route('/root')
 def helloflask():
-    return 'hello flask'
+	return 'hello flask'
+
+#helloflask()
 
 @app.route('/greet/<name>')
 def greet(name):
 	return "Hello %s" % name
+
+#greet("chever")
